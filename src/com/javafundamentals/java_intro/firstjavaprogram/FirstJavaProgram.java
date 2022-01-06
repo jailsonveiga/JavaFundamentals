2b568d40f913436c37c3c58966fb483ac108bfad
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-05 20:18:50 -0500

    First Java Program

---
+++ src/com/javafundamentals/java_intro/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package com.javafundamentals.java_intro.firstjavaprogram;
+
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello EveryOne!!");
+
+    }
+}
