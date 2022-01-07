f4c63cf696de1328a3d93a868ae51e76419055a4
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-06 20:19:39 -0500

    First Java Program With Comments

---
+++ src/com/javafundamentals/java_intro/firstjavaprogram/FirstJavaProgramWithComments.java
@@ -0,0 +1,30 @@
+package com.javafundamentals.java_intro.firstjavaprogram; // the package statement is terminated by semicolon
+
+public class FirstJavaProgramWithComments {
+
+        // This is our main function "method" that belongs to our class
+    public static void main(String[] args) {
+        // public can be accessible from other parts of this program
+        // main() method should always be static
+        // return type is void "is not going to return a value"
+        // passing one parameter (String[] args)
+
+        System.out.println("Hello EveryOne!!");
+        // System is a class.
+        // We can use the dot operator "System." to see the members define in the System
+    }   // out is a field
+}       // println() is a method
+       //  System.out.println() we are calling or executing the print line method
+       //  () This parenthesis we can pass values to our methods
+      //   ("Hello Everyone!!") is a String - String is a sequence of characters
+
+// To execute this program you can click on the play green icon on the top of your IDEA or in a left panel next to the number lines
+// IntelliJ now is building our application, and we can see the result in terminal window
+
+/* Terminal Window
+-------------------------------------
+output:
+
+Hello EveryOne!!
+
+ */
