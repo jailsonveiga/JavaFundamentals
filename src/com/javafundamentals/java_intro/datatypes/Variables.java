5490cd348512ce0b6baf7a34eb32d5cb2260f704
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-07 17:38:22 -0500

    Intro About Variables

---
+++ src/com/javafundamentals/java_intro/datatypes/Variables.java
@@ -0,0 +1,19 @@
+package com.javafundamentals.java_intro.datatypes;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 35;
+
+        int temperature = 20, day = 3;
+
+        int intNum = 55;
+        int num = intNum;
+
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println(day);
+        System.out.println(num);
+
+    }
+}
