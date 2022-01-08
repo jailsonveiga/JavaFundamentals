e3f378bb98b31d78073047eb62020dddc1197753
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-08 16:43:49 -0500

    Primitive Data Type

--- src/com/javafundamentals/java_intro/datatypes/primitive_datatypes/PrimitiveDataTypes.java
+++ src/com/javafundamentals/java_intro/datatypes/primitive_datatypes/PrimitiveDataTypes.java
@@ -1,4 +1,26 @@
 package com.javafundamentals.java_intro.datatypes.primitive_datatypes;
 
 public class PrimitiveDataTypes {
-}
+    public static void main(String[] args) {
+
+        byte age = 20;
+
+        int likesCount = 1_000_000;
+
+        long countBottle = 3_123_456_789L;
+
+        float price = 9.99F;
+
+        char strText = 'S';
+
+        boolean isItTrue = true;
+
+        System.out.println("byte data type: " + age);
+        System.out.println("Integer data type: " + likesCount);
+        System.out.println("long data type: " + countBottle);
+        System.out.println("float data type: " + price);
+        System.out.println("char data type: " + strText);
+        System.out.println("boolean data type: " + isItTrue);
+
+    }
+    }
