2965b0fc871110ac0095e121afa518f7f180c531
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-09 17:55:06 -0500

    Reference Data Type

--- src/com/javafundamentals/java_intro/datatypes/reference_datatypes/ReferenceDataType.java
+++ src/com/javafundamentals/java_intro/datatypes/reference_datatypes/ReferenceDataType.java
@@ -1,2 +1,12 @@
-package com.javafundamentals.java_intro.datatypes.reference_datatypes;public class ReferenceDataType {
+package com.javafundamentals.java_intro.datatypes.reference_datatypes;
+
+import java.util.Date;
+
+public class ReferenceDataType {
+    public static void main(String[] args) {
+
+        Date now = new Date();
+
+        System.out.println("Today's Date: " + now);
+    }
 }
