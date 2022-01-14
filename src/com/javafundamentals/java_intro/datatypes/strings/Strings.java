3eefb063d7d3a7206337c184770b92e968cafdd4
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-14 16:06:24 -0500

    Strings

---
+++ src/com/javafundamentals/java_intro/datatypes/strings/Strings.java
@@ -0,0 +1,18 @@
+package com.javafundamentals.java_intro.datatypes.strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        String message = "Hello";
+        String message1 = "World";
+
+        String messageFullName = message + " " + message1;
+
+        System.out.println(str);
+
+        System.out.println("--------------------------");
+
+        System.out.println(messageFullName);
+    }
+}
